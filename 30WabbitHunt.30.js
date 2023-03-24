e7ffe7a2eb43ee109cc70974bd38ac1dab23cfd8
ocelot <probablyanocelot@gmail.com>
2023-03-24 00:21:18 -0500

    gooseChase go to map, check spawns for rabbit

--- 30WabbitHunt.30.js
+++ 30WabbitHunt.30.js
@@ -3,14 +3,26 @@ game.on("event",function(data){
 	{
 		show_json(data)
 		// let map = data.map
-        // smart_move(data).then(() => {
-            
-        // });
+        gooseChase(data.map, data.name)
 		// Data includes only the "map"
 		// It's up to you to visit all the possible monster spawns
 	}
 });
 
+async function gooseChase(map, goose) {
+    if (character.map != map) await smart_move(map)
+    await doChase(map, goose)
+}
+
+async function doChase(map, goose) {
+    for (let monsterData of map.monsters) {
+        let bottomRight = [monsterData.boundary[1], monsterData.boundary[2]]
+        await smart_move(bottomRight)
+        let found_goose = get_nearest_monster({type:goose})
+        if (!found_goose) continue
+        break
+    }
+}
 // auto_craft("basketofeggs");
 
 // character.s.easterluck <- is the buff
