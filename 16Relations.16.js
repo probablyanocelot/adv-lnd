de00f130abda99343ddb81b4e7e180adc9e24c21
ocelot <probablyanocelot@gmail.com>
2023-03-28 22:06:36 -0500

    better send_equip() WIP

--- 16Relations.16.js
+++ 16Relations.16.js
@@ -224,6 +224,48 @@ function rangerPVP(target) {
 
 // ############### - MERCHANT - ###############
 
+async function send_equip(itemName, player) {
+	// await smart_move(player)
+
+	// if not in inventory, stop
+	// let item_idx = locate_item(itemName)
+	// if (item_idx == -1) return
+	
+	// current solution to find best candidate
+	let matches = []
+	let count = 0
+	let highest_level = 0;
+    for (let item of character_items) {
+        console.log(item)
+
+        if (!item) {
+			count ++
+			continue
+		}
+		if (item.name == itemName) {
+            item.idx = count
+            if (!item.hasOwnProperty('level')) {
+                count++
+                continue
+            }
+            if (item.hasOwnProperty('level') && item.level > highest_level) {
+                highest_level = item.level
+                matches.shift()
+                matches.push(item)
+
+            }
+        }
+        console.log(count)
+        count ++
+        console.log(count)
+        
+
+	}
+	console.log(matches)
+	return matches
+}
+
+
 function sendToMerchant() {
 	if (character.ctype == 'merchant') return;
 	if (!get_player(merchant)) return;
